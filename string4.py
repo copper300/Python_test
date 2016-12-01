21fa669b56e5daec5ff5a2da94cbc2cbc1e57c1c
gaga1080 <gaga1080@gmail.com>
2016-12-01 00:53:42 -0500

    add string4.py

---
+++ string4.py
@@ -0,0 +1,19 @@
+# Python code to demonstrate working of
+# upper(), lower(), swapcase() and title()
+str = "GeeksForGeeks is fOr Geeks"
+
+# Converting string into its lower case
+str1 = str.lower();
+print(" The lower case converted string is : " + str1)
+
+# Converting string into its upper case
+str1 = str.upper()
+print(" The upper case converted string is : " + str1)
+
+# Converting string into its swapped case
+str1 = str.swapcase()
+print(" The swap case converted string is : " + str1)
+
+# Converting string into its title case
+str1 = str.title()
+print(" The title case converted string is : " + str1)
